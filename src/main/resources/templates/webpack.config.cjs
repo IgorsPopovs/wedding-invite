afb25fd035c17c9065848a6466feb171b4e10b6f
igorpopov <popov.igor@inbox.lv>
2026-03-30 01:45:30 +0300

    wrangler config

--- src/main/resources/templates/webpack.config.cjs
+++ src/main/resources/templates/webpack.config.cjs
@@ -1,17 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
+    publicPath: './', // явно указываем путь для ресурсов
   },
   module: {
     rules: [
       {
         test: /\.html$/i,
         loader: 'html-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
