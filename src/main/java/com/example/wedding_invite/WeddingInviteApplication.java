bcb9092d9c5c5299dc0160b01e0670ecb69e6eb1
igorpopov <popov.igor@inbox.lv>
2026-03-29 23:03:27 +0300

    init

---
+++ src/main/java/com/example/wedding_invite/WeddingInviteApplication.java
@@ -0,0 +1,13 @@
+package com.example.wedding_invite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingInviteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingInviteApplication.class, args);
+	}
+//
+}
